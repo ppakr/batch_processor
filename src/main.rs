0a0e3fd682726069649618c97be356a199c65900
ppakr <akira.techap@gmail.com>
2025-04-01 03:08:26 +0700

    Implement directory traversal to find ASC files for conversion

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use clap::Parser;
 use std::path::PathBuf;
+use walkdir::WalkDir;
 
 // CLI tool to convert ASC files to PNGs
+
+// the command 
+// cargo run -- --input-dir ../0925_6225 --output-dir output --mode grayscale
+
+
 #[derive(Parser, Debug)]
 #[command(name = "batch_processor")]
 #[command(about = "Convert ASC DEM files in a folder to images", long_about = None)]
@@ -27,5 +33,16 @@ fn main() -> anyhow::Result<()> {
     println!("Input dir: {:?}", args.input_dir);
     println!("Output dir: {:?}", args.output_dir);
     println!("Mode: {}", args.mode);
+
+    // Iterate over all .asc files in the input directory
+    for entry in WalkDir::new(&args.input_dir).into_iter().filter_map(Result::ok) {
+        let path = entry.path();
+
+        if path.extension().map(|ext| ext == "asc").unwrap_or(false) {
+            println!("Found ASC file: {:?}", path);
+            // Later: call your render + save function here
+        }
+    }
+
     Ok(())
 }
